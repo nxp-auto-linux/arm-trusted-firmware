65068e6ff35ba1c893a4f4974e1b501409a4dc13
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2018-08-05 12:25:42 +0300

    s32g: Compiling skeleton
    Nothing's working, of course, just building BL31 image.
    
    Depending on the compiler optimization level, we might end up with too
    large a bl31 memory footprint, resulting in __BL31_END__ being placed in
    the u-boot section. Pulling addresses around to make images fit even
    with -O0.
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
    Issue: ALB-3452

---
+++ plat/s32g/platform.mk
@@ -0,0 +1,46 @@
+#
+# Copyright 2019 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include lib/xlat_tables_v2/xlat_tables.mk
+
+PLAT_INCLUDES		+= -Iplat/s32g/include \
+			   -Iinclude/common/tbbr \
+			   -Iinclude/plat/arm/common \
+			   -Iinclude/plat/arm/soc/common \
+			   -Iinclude/lib/psci
+PLAT_BL_COMMON_SOURCES	+= plat/s32g/s32g_helpers.S \
+			   plat/s32g/include/plat_macros.S \
+			   drivers/console/aarch64/console.S
+PLAT_BL_COMMON_SOURCES	+= ${XLAT_TABLES_LIB_SRCS}
+
+BL31_SOURCES		+= plat/s32g/s32g275_bl31.c \
+			   plat/common/plat_psci_common.c \
+			   plat/common/plat_gicv3.c \
+			   drivers/arm/gic/v3/gicv3_main.c
+BL31_SOURCES		+= lib/cpus/aarch64/cortex_a53.S
+
+
+# Disable the PSCI platform compatibility layer
+ENABLE_PLAT_COMPAT	:= 0
+
+MULTI_CONSOLE_API	:= 1
+LOAD_IMAGE_V2		:= 1
+USE_COHERENT_MEM	:= 0
+
+# Prepare the stage for BL31-only boot
+PROGRAMMABLE_RESET_ADDRESS	:= 1
+RESET_TO_BL31			:= 1
+COLD_BOOT_SINGLE_CPU		:= 0
+
+### Devel & Debug options ###
+CFLAGS			+= -O0
+# Enable dump of processor register state upon exceptions while running BL31
+CRASH_REPORTING		:= 1
+# As verbose as it can be
+LOG_LEVEL		:= 50
+# Enable simulator-specific workarounds. To be removed after hw is available.
+S32G_VIRTUAL_PLATFORM	:= 1
+$(eval $(call add_define_val,S32G_VIRTUAL_PLATFORM,1))
---
+++ plat/s32g/s32g275_bl31.c
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2019 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch_helpers.h>
+#include <assert.h>
+#include <bl_common.h>
+#include <psci.h>
+#include "platform_def.h"
+
+/* TODO should probably be moved to s32g_psci.c */
+static uintptr_t warmboot_entry;
+
+static entry_point_info_t bl33_image_ep_info;
+
+/* TODO should probably be moved to s32g_psci.c */
+static plat_psci_ops_t s32g_psci_pm_ops = { /* FIXME must implement these */
+	.system_reset = NULL,
+	.pwr_domain_on = NULL,
+	.pwr_domain_on_finish = NULL,
+	.pwr_domain_off = NULL,
+};
+
+const unsigned char s32g_power_domain_tree_desc[] = {
+	PLATFORM_SYSTEM_COUNT,
+	PLATFORM_CORE_COUNT,
+};
+
+static uint32_t s32g_get_spsr_for_bl33_entry(void)
+{
+	uint32_t spsr;
+	unsigned long el_status, mode;
+	unsigned int dbg_current_el;
+
+	/* xDBGx print current EL */
+	dbg_current_el = get_current_el();
+	printf("Current EL is %u\n", dbg_current_el);
+
+	/* figure out what mode we enter the non-secure world */
+	el_status = read_id_aa64pfr0_el1() >> ID_AA64PFR0_EL2_SHIFT;
+	el_status &= ID_AA64PFR0_ELX_MASK;
+
+	mode = (el_status) ? MODE_EL2 : MODE_EL1;
+	assert(mode == MODE_EL2); /* FIXME debugging only! this must go */
+
+	spsr = SPSR_64(mode, MODE_SP_ELX, DISABLE_ALL_EXCEPTIONS);
+
+	return spsr;
+}
+
+entry_point_info_t *bl31_plat_get_next_image_ep_info(uint32_t type)
+{
+	assert(sec_state_is_valid(type));
+
+	return &bl33_image_ep_info;
+}
+
+void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
+		u_register_t arg2, u_register_t arg3)
+{
+	SET_PARAM_HEAD(&bl33_image_ep_info, PARAM_EP, VERSION_1, 0);
+	bl33_image_ep_info.pc = S32G_BL33_IMAGE_BASE;
+	bl33_image_ep_info.spsr = s32g_get_spsr_for_bl33_entry();
+	SET_SECURITY_STATE(bl33_image_ep_info.h.attr, NON_SECURE);
+}
+
+void bl31_plat_arch_setup(void)
+{
+	/* TODO implement this */
+}
+
+void bl31_platform_setup(void)
+{
+	/* TODO implement this */
+}
+
+int plat_core_pos_by_mpidr(u_register_t mpidr)
+{
+	unsigned int cpu_id;
+
+	mpidr &= MPIDR_AFFINITY_MASK;
+
+	if (mpidr & ~(MPIDR_CLUSTER_MASK | MPIDR_CPU_MASK))
+		return -1;
+
+	cpu_id = MPIDR_AFFLVL0_VAL(mpidr);
+
+	if (cpu_id > PLATFORM_MAX_CPU_PER_CLUSTER)
+		return -1;
+
+	return cpu_id;
+}
+
+unsigned int plat_get_syscnt_freq2(void)
+{
+	return COUNTER_FREQUENCY;
+}
+
+const unsigned char *plat_get_power_domain_tree_desc(void)
+{
+	return s32g_power_domain_tree_desc;
+}
+
+int plat_setup_psci_ops(uintptr_t sec_entrypoint,
+			const plat_psci_ops_t **psci_ops)
+{
+	warmboot_entry = sec_entrypoint;
+	*psci_ops = &s32g_psci_pm_ops;
+	return 0;
+}
