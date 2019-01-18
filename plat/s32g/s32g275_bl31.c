d59baf2dbfff765a559522f71d084de23542bcb4
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2019-01-18 04:27:07 +0200

    s32g: Enable RTC interrupt
    Declare RTC IRQ for properly routing after the system is woken up.
    
    In order to resume the suspended system, the RTC will first generate a
    wakeup event with the MC_WKPU, then raise an interrupt which will have
    to be cleared by the core.
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
    Issue: ALB-3452

--- plat/s32g/s32g275_bl31.c
+++ plat/s32g/s32g275_bl31.c
@@ -83,6 +83,8 @@ static uintptr_t rdistif_base_addrs[PLATFORM_CORE_COUNT];
 static const interrupt_prop_t interrupt_props[] = {
 	INTR_PROP_DESC(29, GIC_HIGHEST_SEC_PRIORITY,
 		       INTR_GROUP0, GIC_INTR_CFG_EDGE),
+	INTR_PROP_DESC(153, GIC_HIGHEST_NS_PRIORITY,
+		       INTR_GROUP1S, GIC_INTR_CFG_LEVEL),
 };
 
 const gicv3_driver_data_t s32g275_gic_data = {
