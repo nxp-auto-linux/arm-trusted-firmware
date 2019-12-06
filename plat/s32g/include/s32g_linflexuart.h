03df3da3a489b5ecf99da6a056d250387f62bd94
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2019-12-06 14:17:56 +0200

    s32g: Serial console setup
    Enable serial output from LinFlexD_0. For now, only the standard output
    is linked; the error console is dummy (will be implemented at a later
    point), and serial input is not in our scope.
    Also, UART parameters are hard-coded to match the hardware values that
    other drivers (such as U-Boot's LinFlex UART driver) calculate
    dynamically.
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
    Issue: ALB-4275
    Issue: ALB-4276

--- plat/s32g/include/s32g_linflexuart.h
+++ plat/s32g/include/s32g_linflexuart.h
@@ -12,19 +12,16 @@
 #define S32G_UART_BASE		S32G_LINFLEX0_BASE
 #define S32G_UART_SIZE		S32G_LINFLEX0_SIZE
 #define S32G_UART_BAUDRATE	(115200)
-/* TODO revisit this; for now we'll hard-code the clock/divider settings instead
- * of deriving them
- */
-#define S32G_UART_CLOCK_HZ		(133333333)
+#define S32G_UART_CLOCK_HZ	(133333333)
 
-#define S32G_LINFLEX_LINCR1		(0x0)
-#define S32G_LINFLEX_LINSR		(0x8)
-#define S32G_LINFLEX_UARTCR		(0x10)
-#define S32G_LINFLEX_UARTSR		(0x14)
-#define S32G_LINFLEX_LINIBRR		(0x28)
-#define S32G_LINFLEX_LINFBRR		(0x24)
-#define S32G_LINFLEX_BDRL		(0x38)
-#define S32G_LINFLEX_UARTPTO		(0x50)
+#define S32G_LINFLEX_LINCR1	(0x0)
+#define S32G_LINFLEX_LINSR	(0x8)
+#define S32G_LINFLEX_UARTCR	(0x10)
+#define S32G_LINFLEX_UARTSR	(0x14)
+#define S32G_LINFLEX_LINIBRR	(0x28)
+#define S32G_LINFLEX_LINFBRR	(0x24)
+#define S32G_LINFLEX_BDRL	(0x38)
+#define S32G_LINFLEX_UARTPTO	(0x50)
 
 #ifndef __ASSEMBLY__
 struct console_s32g {
