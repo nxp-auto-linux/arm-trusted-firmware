7f13f9845ffc473849854994533679ee070770bf
Dan Nica <dan.nica@nxp.com>
2020-03-19 10:04:44 +0200

    s32g274a: Enable generic timer
    This is required for the mdelay/udelay() family.
    
    Issue: ALB-4274
    Signed-off-by: Dan Nica <dan.nica@nxp.com>

---
+++ plat/s32g/s32g274a_bl_common.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2020 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "platform_def.h"
+
+unsigned int plat_get_syscnt_freq2(void)
+{
+	return COUNTER_FREQUENCY;
+}
