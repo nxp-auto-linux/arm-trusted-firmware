0d10cbc5223f41874cdcf802304080303981c874
Dan Nica <dan.nica@nxp.com>
2020-06-05 13:04:14 +0300

    s32g: platform.mk: Fix check_dtc_version
    Fix check_dtc_version to also work for 'x.y.z-dirty' versions.
    
    Issue: ALB-4935
    Signed-off-by: Dan Nica <dan.nica@nxp.com>

--- plat/s32g/platform.mk
+++ plat/s32g/platform.mk
@@ -83,7 +83,8 @@ DTC_FLAGS		+= -Wno-unit_address_vs_reg
 
 all: check_dtc_version
 check_dtc_version:
-	$(eval DTC_VERSION_RAW = $(shell $(DTC) --version | cut -f3 -d" "))
+	$(eval DTC_VERSION_RAW = $(shell $(DTC) --version | cut -f3 -d" " \
+							  | cut -f1 -d"-"))
 	$(eval DTC_VERSION = $(shell echo $(DTC_VERSION_RAW) | sed "s/\./0/g"))
 	@if [ ${DTC_VERSION} -lt 10406 ]; then \
 		echo "$(DTC) version must be 1.4.6 or above"; \
