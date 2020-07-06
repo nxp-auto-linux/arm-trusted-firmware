c703c900d8e85c021c1e7b0ca7d45057251208a0
Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>
2020-07-06 15:28:30 +0300

    s32g: Save and restore GIC context
    Issue: ALB-4910
    Signed-off-by: Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>

--- plat/nxp/s32g/include/s32g_lowlevel.h
+++ plat/nxp/s32g/include/s32g_lowlevel.h
@@ -14,6 +14,8 @@ int plat_core_pos_by_mpidr(u_register_t mpidr);
 int plat_is_my_cpu_primary(void);
 void s32g_smp_fixup(void);
 void s32g_gic_setup(void);
+void plat_gic_save(void);
+void plat_gic_restore(void);
 void s32g_early_plat_init(bool skip_ddr_clk);
 
 #endif /* S32G_LOWLEVEL_H */
