45d2f507f09f2690f045340d6e0f4be301b4f69e
Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>
2020-07-07 10:27:09 +0300

    s32g274ardb: Implement system off callback
    Issue: ALB-5115
    Signed-off-by: Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>

--- plat/nxp/s32g/include/s32g_bl_common.h
+++ plat/nxp/s32g/include/s32g_bl_common.h
@@ -13,7 +13,8 @@ void s32g_gic_setup(void);
 void plat_gic_save(void);
 void plat_gic_restore(void);
 void s32g_early_plat_init(bool skip_ddr_clk);
-int prepare_pmic(void);
+int pmic_prepare_for_suspend(void);
+void pmic_system_off(void);
 
 void update_core_state(uint32_t core, uint32_t state);
 bool is_last_core(void);
