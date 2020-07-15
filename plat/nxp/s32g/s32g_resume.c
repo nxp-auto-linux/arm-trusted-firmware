a42b96e5d8368b42ef0fc643f52d4118054119b9
Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>
2020-07-15 06:42:40 +0300

    s32g: Reset RTC during resume procedure
    Reset RTC register to avoid accidental RTC wake-up interrupts.
    
    Issue: ALB-4910
    Signed-off-by: Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>

--- plat/nxp/s32g/s32g_resume.c
+++ plat/nxp/s32g/s32g_resume.c
@@ -6,11 +6,27 @@
 #include "s32g_bl_common.h"
 #include "s32g_linflexuart.h"
 #include "s32g_resume.h"
+#include "s32gen1-wkpu.h"
 #include <bl31/bl31.h>		/* for bl31_warm_entrypoint() */
 #include <lib/el3_runtime/context_mgmt.h>
 #include <lib/el3_runtime/cpu_data.h>
+#include <lib/mmio.h>
 
 void s32g_ncore_isol_cluster0(void);
+
+static void reset_rtc(void)
+{
+	uint32_t rtc = S32G_RTC_BASE;
+	uint32_t rtcs;
+
+	mmio_write_32(rtc + RTC_APIVAL_OFFSET, 0x0);
+	mmio_write_32(rtc + RTC_RTCVAL_OFFSET, 0x0);
+
+	mmio_write_32(rtc + RTC_RTCC_OFFSET, 0x0);
+
+	rtcs = mmio_read_32(rtc + RTC_RTCS_OFFSET);
+	mmio_write_32(rtc + RTC_RTCS_OFFSET, rtcs);
+}
 
 void s32g_resume_entrypoint(void)
 {
@@ -19,6 +35,9 @@ void s32g_resume_entrypoint(void)
 	/* Prepare resume operation */
 	s32g_ncore_isol_cluster0();
 	s32g_early_plat_init(true);
+
+	reset_rtc();
+	s32gen1_wkpu_reset();
 
 	console_s32g_register(S32G_UART_BASE, S32G_UART_CLOCK_HZ,
 			      S32G_UART_BAUDRATE, &console);
