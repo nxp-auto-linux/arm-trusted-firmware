312151ba81b1312164ad609618d00b26349f12bd
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2021-04-23 15:29:53 +0300

    Add partitions to SCMI Reset Domains
    Issue: ALB-7039
    Signed-off-by: Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>

--- include/drivers/nxp/s32g/clk/s32gen1_scmi_rst.h
+++ include/drivers/nxp/s32g/clk/s32gen1_scmi_rst.h
@@ -6,8 +6,10 @@
 #define S32GEN1_SCMI_RST_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 int s32gen1_reset_periph(uint32_t periph_id, bool assert);
+int s32gen1_reset_partition(unsigned int part_id, bool assert_not_deassert);
 
 #endif
 
