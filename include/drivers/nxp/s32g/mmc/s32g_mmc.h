c573951d4fd6f0e8a8e85e2c0efd832d8b2ea14f
Bogdan-Gabriel Roman <bogdan-gabriel.roman@nxp.com>
2021-05-12 00:17:21 +0300

    s32g_storage: detect boot source at runtime
    Whether ATF has booted from QSPI, SD or eMMC can be detected at runtime
    by reading the register BOOT_GPR_BMR1 which is populated with the values
    of RCON[31:0] at every power-on-reset.
    
    Issue: ALB-7120
    Signed-off-by: Bogdan-Gabriel Roman <bogdan-gabriel.roman@nxp.com>

--- include/drivers/nxp/s32g/mmc/s32g_mmc.h
+++ include/drivers/nxp/s32g/mmc/s32g_mmc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 NXP
+ * Copyright 2020-2021 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -7,6 +7,6 @@
 #ifndef S32G274A_MMC_H
 #define S32G274A_MMC_H
 
-int s32g274a_mmc_register(void);
+int s32g274a_mmc_register(uint32_t boot_source);
 
 #endif /* S32G274A_MMC_H */
