0eaebf1bc2e643f609d3bdf8c1831164d7dc8955
Larisa Grigore <larisa.grigore@nxp.com>
2021-05-11 17:29:22 +0300

    s32g: Copy from SD/QSPI only FIP image size
    Loading the entire FIP_MAXIMUM_SIZE from SD/QSPI is a time
    consuming operation. Instead we can load first the FIP header
    and parse it to find the real FIP image size.
    This will decrease the time for bl2_load_images in SD case from
    ~200ms to ~90ms and in QSPI case, from ~725ms to ~237ms.
    
    Issue: ALB-6995
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>

--- plat/nxp/s32g/s32g_storage.c
+++ plat/nxp/s32g/s32g_storage.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 #include <common/bl_common.h>
+#include <common/desc_image_load.h>
 #include <drivers/io/io_driver.h>
 #include <drivers/mmc.h>
 #include <drivers/nxp/s32g/io/io_mmc.h>
@@ -30,9 +31,8 @@ static int s32g_check_mmc_dev(const uintptr_t spec);
 static int s32g_check_fip_dev(const uintptr_t spec);
 static int s32g_check_memmap_dev(const uintptr_t spec);
 
-static const io_block_spec_t fip_mmc_spec = {
+static io_block_spec_t fip_mmc_spec = {
 	.offset = FIP_MMC_OFFSET,
-	.length = ROUND_TO_MMC_BLOCK_SIZE(FIP_MAXIMUM_SIZE),
 };
 
 static const io_block_spec_t fip_memmap_spec = {
@@ -58,9 +58,8 @@ static const io_uuid_spec_t bl33_uuid_spec = {
 	.uuid = UUID_NON_TRUSTED_FIRMWARE_BL33,
 };
 
-static const io_block_spec_t qspi_fip_memmap_spec = {
+static io_block_spec_t qspi_fip_memmap_spec = {
 	.offset = FIP_QSPI_OFFSET,
-	.length = ROUND_TO_MMC_BLOCK_SIZE(FIP_MAXIMUM_SIZE),
 };
 
 static struct plat_io_policy s32g_policies[] = {
@@ -154,18 +153,34 @@ static bool boot_from_qspi(void)
 
 static void set_fip_img_source(struct plat_io_policy *policy)
 {
-	if (boot_from_qspi())
+	bl_mem_params_node_t *fip_params =
+			get_bl_mem_params_node(FIP_IMAGE_ID);
+	image_info_t *image_info = &fip_params->image_info;
+
+	/* We know the real FIP image length only after FIP header
+	 * is read and parsed in bl2_plat_handle_post_image_load.
+	 * This code will be executed twice: the first time when the
+	 * FIP header is read and the image_size will be FIP_HEADER_SIZE
+	 * and second time when the entire FIP is read and image_size
+	 * will be the one obtained in bl2_plat_handle_post_image_load.
+	 */
+	if (boot_from_qspi()) {
+		qspi_fip_memmap_spec.length =
+			image_info->image_size;
 		*policy = (struct plat_io_policy) {
 			.dev_handle = &s32g_memmap_dev_handle,
 			.image_spec = (uintptr_t)&qspi_fip_memmap_spec,
 			.check = s32g_check_memmap_dev,
 		};
-	else
+	} else {
+		fip_mmc_spec.length =
+			ROUND_TO_MMC_BLOCK_SIZE(image_info->image_size);
 		*policy = (struct plat_io_policy) {
 			.dev_handle = &s32g_mmc_dev_handle,
 			.image_spec = (uintptr_t)&fip_mmc_spec,
 			.check = s32g_check_mmc_dev,
 		};
+	}
 }
 
 int plat_get_image_source(unsigned int image_id, uintptr_t *dev_handle,
