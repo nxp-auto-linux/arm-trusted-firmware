f3644782767f47b63ccb6cac9d5d4a905058d588
Larisa Grigore <larisa.grigore@nxp.com>
2021-07-02 11:49:54 +0300

    s32g3: Add SRAMC support
    Issue: ALB-7394
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>

---
+++ plat/nxp/s32g/s32g3/platform.mk
@@ -0,0 +1,14 @@
+#
+# Copyright 2021 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include plat/nxp/s32g/s32g_common.mk
+
+PLAT_BL_COMMON_SOURCES	+= plat/nxp/s32g/s32g3/s32g3_sramc.S \
+			   drivers/nxp/s32g/clk/s32g398a_clk.c \
+
+# Device tree
+DTB_FILE_NAME		?= fsl-s32g398a-rdb.dtb
+FDT_SOURCES             := $(addprefix fdts/, $(patsubst %.dtb,%.dts,$(DTB_FILE_NAME)))
