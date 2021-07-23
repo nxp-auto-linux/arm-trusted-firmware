e8bebc0158c04a81fd2955eaae8a83340b0c83a3
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2021-07-23 14:24:29 +0300

    plat: s32g2: Enable workaround for ERR050764 erratum
    Issue: ALB-7469
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- plat/nxp/s32g/s32g2/platform.mk
+++ plat/nxp/s32g/s32g2/platform.mk
@@ -14,6 +14,7 @@ PLAT_BL_COMMON_SOURCES	+= drivers/nxp/s32g/clk/s32g274a_clk.c \
 ERRATA_S32G2_050481	:= 1
 ERRATA_A53_855873	:= 1
 ERRATA_A53_836870	:= 1
+ERRATA_A53_1530924	:= 1
 
 # Device tree
 DTB_FILE_NAME		?= fsl-s32g274a-rdb.dtb
