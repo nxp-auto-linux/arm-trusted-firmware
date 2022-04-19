3ba2fa9cf0f34f192f7cfd14cc0bdc61a0edd752
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-04-19 09:15:32 +0300

    plan: nxp: Add S32G2 BlueBox3 platform
    Issue: ALB-8797
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ plat/nxp/s32/s32g/s32g2/s32g274abluebox3/platform.mk
@@ -0,0 +1,15 @@
+#
+# Copyright 2022 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include plat/nxp/s32/s32g/s32g2/s32g2.mk
+
+S32_BOARD_PATH		:= ${PLAT_SOC_PATH}/s32g274abluebox3
+
+PLAT_INCLUDES          += -I${S32_BOARD_PATH}/include \
+
+PLAT_BL_COMMON_SOURCES +=      ${PLAT_SOC_PATH}/s32g2_vr5510.c \
+
+DTB_FILE_NAME		?= fsl-s32g274a-bluebox3.dtb
