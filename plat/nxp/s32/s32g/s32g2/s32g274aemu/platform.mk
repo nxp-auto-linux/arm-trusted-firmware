d2ad9ed5a751d80c5e5d2f501454c39116b5046e
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-04-19 09:46:41 +0300

    plat: nxp: Add S32G274A EMU platform
    Issue: ALB-8797
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ plat/nxp/s32/s32g/s32g2/s32g274aemu/platform.mk
@@ -0,0 +1,15 @@
+#
+# Copyright 2022 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+S32CC_EMU = 1
+
+include plat/nxp/s32/s32g/s32g2/s32g2.mk
+
+S32_BOARD_PATH		:= ${PLAT_SOC_PATH}/s32g274aemu
+
+PLAT_INCLUDES		+= -I${S32_BOARD_PATH}/include \
+
+DTB_FILE_NAME		?= fsl-s32g274a-emu.dtb
