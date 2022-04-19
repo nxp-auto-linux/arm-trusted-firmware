bc147e06f139b31efab02e77a84ef17b273621a5
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-04-19 09:40:16 +0300

    plat: nxp: Add S32G399A EMU platform
    Issue: ALB-8797
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ plat/nxp/s32/s32g/s32g3/s32g399aemu/platform.mk
@@ -0,0 +1,15 @@
+#
+# Copyright 2022 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+S32CC_EMU = 1
+
+include plat/nxp/s32/s32g/s32g3/s32g3.mk
+
+S32_BOARD_PATH		:= ${PLAT_SOC_PATH}/s32g399aemu
+
+PLAT_INCLUDES		+= -I${S32_BOARD_PATH}/include \
+
+DTB_FILE_NAME		?= fsl-s32g399a-emu.dtb
