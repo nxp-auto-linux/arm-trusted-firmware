bf111198f6ccca5dd13184be93c63ad7469dbba8
Ciprian Costea <ciprianmarian.costea@nxp.com>
2022-05-09 17:24:24 +0300

    dt-bindings: Add definitions for reserved ddr errata memory region
    Issue: ALB-8781
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>

---
+++ include/dt-bindings/ddr-errata/s32-ddr-errata.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2022 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _S32_DDR_ERRATA_H_
+#define _S32_DDR_ERRATA_H_
+
+#define DDR_ERRATA_REGION_BASE	0x85000000
+#define DDR_ERRATA_REGION_SIZE	0x1000
+
+#endif
