d1938e57115a1f849b1d9af7b929455f5a41c25f
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-05-16 15:06:21 +0300

    dts: phy: Add dt constants for s32cc device trees
    Issue: ALB-8823
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ include/dt-bindings/phy/phy.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause) */
+/*
+ * PHY bindings
+ *
+ * Copyright 2022 NXP
+ */
+#ifndef DT_BINDINGS_PHY_H
+#define DT_BINDINGS_PHY_H
+
+#define PHY_TYPE_XPCS	7
+
+#endif
