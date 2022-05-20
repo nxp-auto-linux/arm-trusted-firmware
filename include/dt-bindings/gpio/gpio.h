18e5c5b9617777f57b9726acf849191d371b05dc
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2022-05-20 08:01:02 +0300

    dts: gpio: Add dt constants for s32cc device trees
    Issue: ALB-8797
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ include/dt-bindings/gpio/gpio.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause) */
+/*
+ * GPIO bindings
+ *
+ * Copyright 2022 NXP
+ */
+
+#ifndef DT_BINDINGS_GPIO_H
+#define DT_BINDINGS_GPIO_H
+
+#define GPIO_ACTIVE_HIGH	0
+#define GPIO_OPEN_DRAIN		4
+
+#endif
