69755fb71d379ce92bde24f3f669e3f7e9a846a7
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2022-08-30 17:53:09 +0300

    ddr: Add CUSTOM_DDR_DRV make parameter for external driver
    Added the handling of CUSTOM_DDR_DRV parameter, which should
    point to a folder that contains the DDR code in the state
    in which it is generated by DDR Tool.
    
    With the CUSTOM_DDR_DRV passed to the make command, the
    "import_ddr_drv" rule should be invoked first, such that
    the DDR files are changed and adapted to ATF, and
    the compilation command afterwards.
    
    It can be used as following, for example:
    make CROSS_COMPILE=... ARCH=aarch64 PLAT=... BL33=...
    	CUSTOM_DDR_DRV=<relative_path_to_ddr_code> import_ddr_drv
    make CROSS_COMPILE=... ARCH=aarch64 PLAT=... BL33=...
    »       CUSTOM_DDR_DRV=<relative_path_to_ddr_code>
    
    Issue: ALB-9210
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

--- plat/nxp/s32/s32_ddr.mk
+++ plat/nxp/s32/s32_ddr.mk
@@ -6,8 +6,14 @@
 
 DDR_DRV = ${S32_DRIVERS}/ddr
 
+ifneq (${CUSTOM_DDR_DRV},)
+COMMON_DDR_DRV	:= ${CUSTOM_DDR_DRV}
+DDR_UTILS_FILE	:= ${CUSTOM_DDR_DRV}/ddr_utils.h
+PLAT_DDR_DRV	:= ${CUSTOM_DDR_DRV}
+else
 COMMON_DDR_DRV	= ${DDR_DRV}
 PLAT_DDR_DRV	= ${DDR_DRV}/${S32_PLAT_SOC}
+endif
 
 DDR_DRV_SRCS += \
 	${PLAT_DDR_DRV}/ddrc_cfg.c \
@@ -22,3 +28,18 @@ DDR_DRV_SRCS += \
 	${COMMON_DDR_DRV}/ddrss_cfg.c \
 	${COMMON_DDR_DRV}/imem_cfg.c \
 	${DDR_DRV}/ddr_density.c \
+
+# If CUSTOM_DDR_DRV is set, this target modifies the ddr_utils.h file
+# to include the necessary headers and macros and remove the other ones
+ifneq (${CUSTOM_DDR_DRV},)
+import_ddr_drv: ${DDR_UTILS_FILE}
+	@${SED} -ie "/#ifndef CONFIG_S32_GEN1/,/#endif/d" $<
+	@${SED} -ie "/#include <stdbool.h>/d" $<
+	@${SED} -ie "/#include <stdlib.h>/d" $<
+	@${SED} -ie "/#include <ddr_plat.h>/d" $<
+	@${SED} -ie "/#include <lib\/mmio.h>/d" $<
+	@${SED} -ie "/#define DDR_UTILS_H_/a #include <lib\/mmio.h>" $<
+	@${SED} -ie "/#define DDR_UTILS_H_/a #include <ddr_plat.h>" $<
+	@${SED} -ie "s/\(#define RETENTION_ADDR\).*STNDBY_RAM_BASE/\1 BL31SSRAM_CSR_BASE/g" $<
+	@${ECHO} "  EDITED  $<"
+endif
