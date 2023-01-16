ddadbed5158add5f91a8388f62ea3f2222d768fe
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-01-16 08:47:07 +0200

    plat: s32: Add s32g3xxaevb3 board
    Issue: ALB-9666
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ plat/nxp/s32/s32g/s32g3/s32g3xxaevb3/platform.mk
@@ -0,0 +1,13 @@
+#
+# Copyright 2023 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include plat/nxp/s32/s32g/s32g3/s32g3.mk
+
+S32_BOARD_PATH		:= ${PLAT_SOC_PATH}/s32g3xxaevb3
+
+PLAT_INCLUDES		+= -I${S32_BOARD_PATH}/include \
+
+DTB_FILE_NAME		?= s32g3xxa-evb3.dtb
