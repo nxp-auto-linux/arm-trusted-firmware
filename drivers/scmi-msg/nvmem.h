fce3541273125979384263e929e388a6c627bd77
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2023-05-24 17:40:32 +0300

    scmi: Add NVMEM over SCMI Protocol interface file
    Issue: ALB-9554
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

---
+++ drivers/scmi-msg/nvmem.h
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2023 NXP
+ */
+
+#ifndef SCMI_MSG_NVMEM_H
+#define SCMI_MSG_NVMEM_H
+
+#include <stdint.h>
+
+#define SCMI_PROTOCOL_VERSION_NVMEM	0x10000U
+
+/*
+ * Identifiers of the SCMI NVMEM Protocol commands
+ */
+enum scmi_nvmem_command_id {
+	SCMI_NVMEM_READ_CELL = 0x3,
+};
+
+/*
+ * NVMEM Read Cell
+ */
+struct scmi_nvmem_read_cell_a2p {
+	uint32_t offset;
+	uint32_t bytes;
+};
+
+struct scmi_nvmem_read_cell_p2a {
+	int32_t status;
+	uint32_t value;
+	uint32_t bytes;
+};
+
+#endif /* SCMI_MSG_NVMEM_H */
