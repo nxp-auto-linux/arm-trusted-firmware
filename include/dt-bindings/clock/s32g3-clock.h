ef8f916cd02a5c12638d28fcb275f996806ed78e
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-05-29 14:40:36 +0300

    dt-bindings/clock: Correct S32G3 clock ids
    Issue: ALB-10024
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- include/dt-bindings/clock/s32g3-clock.h
+++ include/dt-bindings/clock/s32g3-clock.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2021 NXP
+ * Copyright 2021, 2023 NXP
  */
 
 #ifndef __DT_BINDINGS_CLOCK_S32G3_H
@@ -8,9 +8,10 @@
 
 #include <dt-bindings/clock/s32g-clock.h>
 
-#define S32G_CLK_MC_CGM6_MUX0		S32G_CLK(18)
-#define S32G_CLK_MC_CGM6_MUX1		S32G_CLK(19)
-#define S32G_CLK_MC_CGM6_MUX2		S32G_CLK(20)
-#define S32G_CLK_MC_CGM6_MUX3		S32G_CLK(21)
+#define S32G3_CLK(X)			(S32G_CLK_LAST + (X))
+#define S32G_CLK_MC_CGM6_MUX0		S32G3_CLK(0)
+#define S32G_CLK_MC_CGM6_MUX1		S32G3_CLK(1)
+#define S32G_CLK_MC_CGM6_MUX2		S32G3_CLK(2)
+#define S32G_CLK_MC_CGM6_MUX3		S32G3_CLK(3)
 
 #endif //__DT_BINDINGS_CLOCK_S32G3_H
