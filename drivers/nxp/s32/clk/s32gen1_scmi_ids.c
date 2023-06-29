556d4570054da5fa88056dbecf5f1fd5ea140edd
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2023-06-29 09:41:41 +0300

    dt-bindings/clock: s32cc: Add DBG clocks
    Issue: ALB-9660
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- drivers/nxp/s32/clk/s32gen1_scmi_ids.c
+++ drivers/nxp/s32/clk/s32gen1_scmi_ids.c
@@ -174,6 +174,10 @@ struct s32gen1_scmi_clk cc_scmi_clk[] = {
 			 S32GEN1_CLK_XBAR_DIV3, "ctu_module"),
 	SCMI_ARRAY_ENTRY(S32GEN1_SCMI_CLK_CTU_CTU,
 			 S32GEN1_CLK_PER, "ctu_ctu"),
+	SCMI_ARRAY_ENTRY(S32GEN1_SCMI_CLK_DBG_SYS4,
+			 S32GEN1_CLK_XBAR_DIV2, "dbg_sys4"),
+	SCMI_ARRAY_ENTRY(S32GEN1_SCMI_CLK_DBG_SYS2,
+			 S32GEN1_CLK_XBAR, "dbg_sys2"),
 };
 
 static int compound2clkid(uint32_t scmi_clk_id, uint32_t *clk_id)
